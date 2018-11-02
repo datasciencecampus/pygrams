2b8b97ca3668bf45531bf446e932fe7e342caa1f
IanGrimstead <Ian.Grimstead@ons.gov.uk>
2018-11-02 13:40:27 +0000

    Added output of term counts (completes #75)

---
+++ scripts/utils/datesToPeriods.py
@@ -0,0 +1,42 @@
+import numpy as np
+from scipy.sparse import csr_matrix, vstack, isspmatrix_csr
+from tqdm import tqdm
+
+
+def tfidf_with_dates_to_weekly_term_counts(term_value_array, uspto_week_dates):
+    number_of_rows, number_of_terms = term_value_array.shape
+    week_counts_csr = None
+
+    if not isspmatrix_csr(term_value_array):
+        term_value_array = csr_matrix(term_value_array)
+
+    current_week = int(uspto_week_dates[0])
+    current_week_counts_csr = csr_matrix((1, number_of_terms), dtype=np.int32)
+    week_totals = []
+    week_dates = []
+    week_total = 0
+
+    for current_row_index in tqdm(range(number_of_rows), 'Counting terms per week', unit='patent'):
+        new_week = int(uspto_week_dates[current_row_index])
+
+        while new_week > current_week:
+            week_counts_csr = vstack([week_counts_csr, current_week_counts_csr],
+                                     format='csr') if week_counts_csr is not None else current_week_counts_csr
+            week_totals.append(week_total)
+            week_dates.append(current_week)
+            current_week_counts_csr = csr_matrix((1, number_of_terms), dtype=np.int32)
+            current_week += 1
+            if (current_week % 100) > 53:
+                current_week += 100 - 53  # next year, so add 100 but remove the "used" weeks
+            week_total = 0
+
+        current_row_as_counts = term_value_array[current_row_index, :] > 0
+        current_week_counts_csr += current_row_as_counts
+        week_total += 1
+
+    week_counts_csr = vstack([week_counts_csr, current_week_counts_csr],
+                             format='csr') if week_counts_csr is not None else current_week_counts_csr
+    week_totals.append(week_total)
+    week_dates.append(current_week)
+
+    return week_counts_csr, week_totals, week_dates
---
+++ tests/utils/test_datesToPeriods.py
@@ -0,0 +1,78 @@
+import unittest
+import numpy as np
+import numpy.testing as npt
+from scripts.utils.datesToPeriods import tfidf_with_dates_to_weekly_term_counts
+
+
+class test_usptoDatesToPeriods(unittest.TestCase):
+
+    @staticmethod
+    def run_test_with_conversion(combined_array):
+        numpy_matrix = np.array(combined_array)
+        tfidf_matrix = np.array(numpy_matrix[:, 1:])
+        publication_week_dates = np.array(numpy_matrix[:, 0])
+        return tfidf_with_dates_to_weekly_term_counts(tfidf_matrix, publication_week_dates)
+
+    def test_week_single_entry(self):
+        tfidf = [
+            [200801, 0, 0.3, 0],
+        ]
+        expected_term_counts = np.array([
+            [0, 1, 0],
+        ])
+        expected_term_totals = [1]
+        expected_week_dates = [200801]
+        actual_term_counts, actual_term_totals, actual_week_dates = self.run_test_with_conversion(tfidf)
+        npt.assert_array_equal(expected_term_counts, actual_term_counts.todense())
+        npt.assert_array_equal(expected_term_totals, actual_term_totals)
+        npt.assert_array_equal(expected_week_dates, actual_week_dates)
+
+    def test_week_combining_and_gaps(self):
+        combined_array = [
+            [200801, 0, 0.3, 0],
+            [200801, 0, 0, 0],
+            [200802, 0, 0, 2.3],
+            [200804, 0.1, 0.3, 0],
+            [200804, 0.2, 0, 0.1],
+            [200806, 0, 0.3, 0],
+        ]
+        expected_term_counts = np.array([
+            [0, 1, 0],
+            [0, 0, 1],
+            [0, 0, 0],
+            [2, 1, 1],
+            [0, 0, 0],
+            [0, 1, 0]
+        ])
+        expected_term_totals = [2, 1, 0, 2, 0, 1]
+        expected_week_dates = [200801, 200802, 200803, 200804, 200805, 200806]
+        actual_term_counts, actual_term_totals, actual_week_dates = self.run_test_with_conversion(combined_array)
+        npt.assert_array_equal(expected_term_counts, actual_term_counts.todense())
+        npt.assert_array_equal(expected_term_totals, actual_term_totals)
+        npt.assert_array_equal(expected_week_dates, actual_week_dates)
+
+    def test_week_combining_and_split_across_years(self):
+        combined_array = [
+            [200850, 0, 0.3, 0],
+            [200852, 0, 0, 0],
+            [200852, 0, 0, 2.3],
+            [200902, 0.1, 0.3, 0],
+            [200902, 0.2, 0, 0.1],
+            [200904, 0, 0.3, 0],
+        ]
+        expected_term_counts = np.array([
+            [0, 1, 0],
+            [0, 0, 0],
+            [0, 0, 1],
+            [0, 0, 0],
+            [0, 0, 0],
+            [2, 1, 1],
+            [0, 0, 0],
+            [0, 1, 0]
+        ])
+        expected_week_dates = [200850, 200851, 200852, 200853, 200901, 200902, 200903, 200904]
+        expected_term_totals = [1, 0, 2, 0, 0, 2, 0, 1]
+        actual_term_counts, actual_term_totals, actual_week_dates = self.run_test_with_conversion(combined_array)
+        npt.assert_array_equal(expected_term_counts, actual_term_counts.todense())
+        npt.assert_array_equal(expected_term_totals, actual_term_totals)
+        npt.assert_array_equal(expected_week_dates, actual_week_dates)
