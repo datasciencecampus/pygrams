c02dc2006b17176d8469816fef2e309514aca2d1
Thanasis <thanasis.anthopoulos@ons.gov.uk>
2019-02-22 17:42:22 +0000

    resolved an error spotted in terms counts data

--- scripts/data_factory.py
+++ scripts/data_factory.py
@@ -1,3 +1,5 @@
+import csv
+import ast
 import pandas as pd
 
 
@@ -7,6 +9,6 @@ def get(doc_source_file_name):
     elif doc_source_file_name.endswith('.xls'):
         return pd.read_excel(doc_source_file_name)
     elif doc_source_file_name.endswith('.csv'):
-        return pd.read_csv(doc_source_file_name, engine='python', error_bad_lines=False)
+        return pd.read_csv(doc_source_file_name, engine='python', error_bad_lines=False, skipinitialspace=True)
     elif doc_source_file_name.endswith('.xlsx'):
         return pd.read_excel(doc_source_file_name)
---
+++ tests/test_data_factory.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import unittest
+from scripts import FilePaths
+from scripts import data_factory as factory
+
+
+class TestDataFactory(unittest.TestCase):
+
+    def setUp(self):
+        self.__df = pd.read_pickle(FilePaths.us_patents_random_100_pickle_name)
+        self.__df = self.__df.reset_index()
+
+    def test_reads_xls(self):
+        df = factory.get('data/USPTO-random-100.xls')
+        self.assertListEqual(list(self.__df['abstract']), list(df['abstract']))
+
+    def test_reads_xlsx(self):
+        df = factory.get('data/USPTO-random-100.xlsx')
+        self.assertListEqual(list(self.__df['abstract']), list(df['abstract']))
+
+    def test_reads_csv(self):
+        df = factory.get('data/USPTO-random-100.csv')
+        self.assertListEqual(list(self.__df['abstract']), list(df['abstract']))
+
+    def test_reads_pickles(self):
+        df = factory.get('data/USPTO-random-100.pkl.bz2')
+        self.assertEquals(len(df['abstract']), 100)
+
+if __name__ == '__main__':
+    unittest.main()
