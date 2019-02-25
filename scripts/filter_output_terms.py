a5cb34e4b96536c0d1f073c22b3e90c30ad51e6d
Thanasis <thanasis.anthopoulos@ons.gov.uk>
2019-02-25 13:17:20 +0000

    emily's placeholder

--- scripts/filter_output_terms.py
+++ scripts/filter_output_terms.py
@@ -1,10 +1,15 @@
 class FilterTerms(object):
-    def __init__(self, term_score_tuples, nterms):
+    def __init__(self, tfidf_ngrams, user_ngrams, binary=False, threshold = None):
         print("filter terms")
-        self.__nterms = nterms
-        self.__term_score_tuples = term_score_tuples
+        self.__user_ngrams = user_ngrams
+        self.__tfidf_ngrams = tfidf_ngrams
+        self.__ngrams_weights_vect = None
 
     #embeddings best to be blended in the mask! ie. data[i] *= cosine_distance between domain words and ngram_average
     @property
-    def term_score_tuples(self):
-        return self.__term_score_tuples[:self.__nterms]
+    def ngrams_weights_vect(self):
+        return self.__ngrams_weights_vect
+
+    def calculate_distances(self):
+        print()
+        #populate this one: self.__ngrams_weights_vect
