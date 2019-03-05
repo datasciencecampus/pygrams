78bffb350ed55f0f303ac3a32407b374c70f0e55
IanGrimstead <Ian.Grimstead@ons.gov.uk>
2019-03-05 17:00:05 +0000

    Forced sync with pygrams2

--- scripts/pipeline.py
+++ scripts/pipeline.py
@@ -1,49 +1,87 @@
+import os
+
 import scripts.data_factory as datafactory
 import scripts.output_factory as output_factory
 from scripts.documents_filter import DocumentsFilter
 from scripts.documents_weights import DocumentsWeights
-from scripts.filter_output_terms import FilterTerms
+from scripts.filter_terms import FilterTerms
 from scripts.text_processing import LemmaTokenizer
 from scripts.tfidf_mask import TfidfMask
 from scripts.tfidf_reduce import TfidfReduce
 from scripts.tfidf_wrapper import TFIDF
+from scripts.utils import utils
+from scripts.utils.date_utils import year2pandas_earliest_date, year2pandas_latest_date
 
 
 class Pipeline(object):
-    def __init__(self, data_filename, filter_columns, cpc=None, pick_method='sum', max_n=3, min_n=1,
-                 normalize_rows=False, text_header='abstract', term_counts=False, dates_header=None,
-                 pickled_tf_idf=False, filter_by='union', time=False, citation_dict=None, nterms=25, max_df=0.1):
-        # load data
-        df = datafactory.get(data_filename)
+    def __init__(self, data_filename, docs_mask_dict, pick_method='sum', ngram_range=(1, 3),
+                 normalize_rows=False, text_header='abstract', term_counts=False,
+                 pickled_tf_idf=False, max_df=0.1, user_ngrams=None):
 
-        # calculate tf-idf mat
+        # load data
+        self.__data_filename = data_filename
+
+        doc_dates = docs_mask_dict['dates']
+        year_from = doc_dates[0]
+        year_to = doc_dates[1]
+        month_from = doc_dates[2]
+        month_to = doc_dates[3]
+
+        date_from = year2pandas_earliest_date(year_from, month_from)
+        date_to = year2pandas_latest_date(year_to, month_to)
+        self.__date_range = [date_from, date_to]
+        self.__time = docs_mask_dict['time']
+
+        df = datafactory.get(data_filename)
+        self.__pick_method = pick_method
+        # calculate or fetch tf-idf mat
         if pickled_tf_idf:
             print('read from pickle')
             self.__tfidf_obj = None
         else:
-            self.__tfidf_obj = TFIDF(docs_df=df, ngram_range=(min_n, max_n), max_document_frequency=max_df,
+            self.__tfidf_obj = TFIDF(docs_df=df, ngram_range=ngram_range, max_document_frequency=max_df,
                                      tokenizer=LemmaTokenizer(), text_header=text_header)
-        # docs subset
-        doc_ids = DocumentsFilter(df, filter_columns, filter_by, cpc).doc_indices
 
-        # doc weights
-        doc_weights = DocumentsWeights(time, citation_dict).weights
+        # docs weights( column, dates subset + time, citations etc.)
+        doc_filters = DocumentsFilter(df, docs_mask_dict).doc_weights
+        doc_weights = DocumentsWeights(df, docs_mask_dict['time'], docs_mask_dict['cite'],
+                                       docs_mask_dict['dates'][-1], text_header=text_header,
+                                       norm_rows=normalize_rows).weights
+        doc_weights = [a * b for a, b in zip(doc_filters, doc_weights)]
+
+        # term weights - embeddings
+        filter_terms_obj = FilterTerms(self.__tfidf_obj.feature_names, user_ngrams,
+                                       file_name=os.path.join('data', 'embeddings', 'glove', 'w2v_glove.6B.50d.txt'))
+        term_weights = filter_terms_obj.ngram_weights_vec
 
         # tfidf mask ( doc_ids, doc_weights, embeddings_filter will all merge to a single mask in the future)
-        tfidf_mask = TfidfMask(self.__tfidf_obj, doc_weights,
-                               norm_rows=normalize_rows, min_ngram_length=min_n, max_ngram_length=max_n).tfidf_mask
+        tfidf_mask_obj = TfidfMask(self.__tfidf_obj, ngram_range=ngram_range, uni_factor=0.8)
+        tfidf_mask_obj.update_mask(doc_weights, term_weights)
+        tfidf_mask = tfidf_mask_obj.tfidf_mask
 
-        self.__tfidf_reduce_obj = TfidfReduce(self.__tfidf_obj, tfidf_mask)
-        self.__term_counts_mat = None
+        # mask the tfidf matrix
+        tfidf_matrix = self.__tfidf_obj.tfidf_matrix
+        tfidf_masked = tfidf_mask.multiply(tfidf_matrix)
+        tfidf_masked = utils.remove_all_null_rows(tfidf_masked)
+
+        print(f'Processing TFIDF matrix of {tfidf_masked.shape[0]:,} / {tfidf_matrix.shape[0]:,} documents')
+
+        self.__tfidf_reduce_obj = TfidfReduce(tfidf_masked, self.__tfidf_obj.feature_names)
+        self.__term_counts_data = None
         if term_counts:
-            self.__term_counts_mat = self.__tfidf_reduce_obj.create_terms_count(df, dates_header)
+            self.__term_counts_data = self.__tfidf_reduce_obj.create_terms_count(df, docs_mask_dict['dates'][-1])
         # if other outputs
-        term_score_tuples = self.__tfidf_reduce_obj.extract_ngrams_from_docs_set(doc_ids, pick_method)
-        filter_output_obj = FilterTerms(term_score_tuples, nterms=nterms)
-        self.__term_score_tuples = filter_output_obj.term_score_tuples
+        self.__term_score_tuples = self.__tfidf_reduce_obj.extract_ngrams_from_docset(pick_method)
 
     def output(self, output_types, wordcloud_title=None, outname=None, nterms=50):
+
         for output_type in output_types:
             output_factory.create(output_type, self.__term_score_tuples, wordcloud_title=wordcloud_title,
                                   tfidf_reduce_obj=self.__tfidf_reduce_obj, name=outname,
-                                  nterms=nterms, term_counts_mat=self.__term_counts_mat)
+                                  nterms=nterms, term_counts_data=self.__term_counts_data,
+                                  tfidf_obj=self.__tfidf_obj, date_range=self.__date_range, pick=self.__pick_method,
+                                  doc_pickle_file_name=self.__data_filename, time=self.__time)
+
+    @property
+    def term_score_tuples(self):
+        return self.__term_score_tuples
--- scripts/tfidf_reduce.py
+++ scripts/tfidf_reduce.py
@@ -1,7 +1,7 @@
 import numpy as np
 from tqdm import tqdm
 
-from scripts.utils.datesToPeriods import tfidf_with_dates_to_weekly_term_counts
+from scripts.utils.date_utils import tfidf_with_dates_to_weekly_term_counts
 
 
 class TfidfReduce(object):
@@ -47,7 +47,7 @@ class TfidfReduce(object):
             pick_func = np.average
         elif pick_method == 'max':
             pick_func = np.max
-        elif pick_method == 'sum':
+        else:
             pick_func = np.sum
 
         ngrams_scores_tuple = []
@@ -60,68 +60,6 @@ class TfidfReduce(object):
             end_idx_inptr = self.__tfidf_masked.indptr[ngram_index + 1]
 
             non_zero_values_term = self.__tfidf_masked.data[start_idx_inptr:end_idx_inptr]
-            if len(non_zero_values_term) > 0:
-                pick_value = pick_func(non_zero_values_term)
-
-                if np.isnan(pick_value):
-                    pick_value = 0
-
-                ngrams_scores_tuple.append((pick_value, ngram))
-
-        ngrams_scores_tuple.sort(key=lambda tup: -tup[0])
-        return ngrams_scores_tuple
-
-    # obsolete
-    def extract_ngrams_from_docs_set(self, docs_subset, pick_method, verbose=True):
-        if docs_subset is None or len(docs_subset)==0:
-            print(f'Processing TFIDF of {self.__tfidf_matrix.shape[0]:,} documents')
-
-        if self.__tfidf_matrix.shape[0] == 0:
-            print('...skipping as 0 documents...')
-            return []
-
-        # pick filter
-        tfidf_csc_matrix = self.__tfidf_matrix.tocsc()
-
-        if pick_method == 'median':
-            pick_func = np.median
-        elif pick_method == 'avg':
-            pick_func = np.average
-        elif pick_method == 'max':
-            pick_func = np.max
-        elif pick_method == 'sum':
-            pick_func = np.sum
-
-        # for i, v in enumerate(time_weights):
-        #     self.__tfidf_matrix.data[self.__tfidf_matrix.indptr[i]:self.__tfidf_matrix.indptr[i + 1]] *= v
-
-        ngrams_scores_tuple = []
-        feature_iterator = self.__feature_names
-        if verbose:
-            feature_iterator = tqdm(feature_iterator, leave=False, desc='Searching TFIDF', unit='ngram')
-
-        for ngram_index, ngram in enumerate(feature_iterator):
-
-            start_idx_inptr = tfidf_csc_matrix.indptr[ngram_index]
-            end_idx_inptr = tfidf_csc_matrix.indptr[ngram_index + 1]
-
-            non_zero_values_term = tfidf_csc_matrix.data[start_idx_inptr:end_idx_inptr]
-            non_zero_values_mask = self.__tfidf_mask[start_idx_inptr:end_idx_inptr]
-            non_zero_values_term = [x*y for x,y in zip(non_zero_values_term, non_zero_values_mask)]
-
-            if docs_subset is not None and len(docs_subset)>0:
-
-                row_indices_term = tfidf_csc_matrix.indices[start_idx_inptr:end_idx_inptr]
-                non_zero_values_term_set = []
-
-                indices_idx = 0
-                for doc_idx in docs_subset:
-                    while indices_idx < len(row_indices_term) and row_indices_term[indices_idx] <= doc_idx:
-                        if row_indices_term[indices_idx] == doc_idx:
-                            non_zero_values_term_set.append(non_zero_values_term[indices_idx])
-                        indices_idx += 1
-                non_zero_values_term = non_zero_values_term_set
-
             if len(non_zero_values_term) > 0:
                 pick_value = pick_func(non_zero_values_term)
 
