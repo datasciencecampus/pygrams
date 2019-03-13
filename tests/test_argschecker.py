c73ff31c5cdb88221623aa31b5aa91dbfb4fca3a
user624086 <bernard.peat@ons.gov.uk>
2019-03-13 14:15:20 +0000

    argschecker datetime unit tests working

--- tests/test_argschecker.py
+++ tests/test_argschecker.py
@@ -1,4 +1,6 @@
 import unittest
+import pandas as pd
+from pandas import Timestamp
 from unittest import mock
 from unittest.mock import MagicMock
 
@@ -52,4 +54,80 @@ class TestArgsChecker(unittest.TestCase):
             self.fail('Should have detected erroneous date format')
 
         except PygramsException as pe:
-            self.assertEqual(f"date_to defined as '{bogus_date}' which is not in YYYY/MM/DD format", pe.message, 'Messages do not match')
+            self.assertEqual(f"date_to defined as '{bogus_date}' which is not in YYYY/MM/DD format",
+                             pe.message, 'Messages do not match')
+
+    @mock.patch("scripts.utils.argschecker.path", create=True)
+    def test_from_date_correct_format(self, mock_path):
+        self.setUpTest(mock_path)
+        self.args.date_from = '2018/03/10'
+        args_checker = ArgsChecker(self.args, self.args_default)
+
+        args_checker.checkargs()
+
+    @mock.patch("scripts.utils.argschecker.path", create=True)
+    def test_from_date_incorrect_format(self, mock_path):
+        self.setUpTest(mock_path)
+        bogus_date = '03/11'
+        self.args.date_from = bogus_date
+        args_checker = ArgsChecker(self.args, self.args_default)
+
+        try:
+            args_checker.checkargs()
+            self.fail('Should have detected erroneous date format')
+
+        except PygramsException as pe:
+            self.assertEqual(f"date_from defined as '{bogus_date}' which is not in YYYY/MM/DD format",
+                             pe.message, 'Messages do not match')
+
+    @mock.patch("scripts.utils.argschecker.path", create=True)
+    def test_from_date_not_after_to_date(self, mock_path):
+        self.setUpTest(mock_path)
+        self.args.date_from = '2018/03/10'
+        self.args.date_to = '2019/03/10'
+        args_checker = ArgsChecker(self.args, self.args_default)
+
+        args_checker.checkargs()
+
+    @mock.patch("scripts.utils.argschecker.path", create=True)
+    def test_from_date_after_to_date(self, mock_path):
+        self.setUpTest(mock_path)
+        self.args.date_from = '2019/03/11'
+        self.args.date_to = '2019/03/10'
+        args_checker = ArgsChecker(self.args, self.args_default)
+
+        try:
+            args_checker.checkargs()
+            self.fail('Should have detected date_from > date_to')
+
+        except PygramsException as pe:
+            self.assertEqual(f"date_from '{self.args.date_from}' cannot be after date_to '{self.args.date_to}'",
+                             pe.message, 'Dates do not match')
+
+    @mock.patch("scripts.utils.argschecker.path", create=True)
+    def test_get_docs_mask_dict(self, mock_path):
+        expected_date_from = '2019/03/10'
+        expected_date_to = '2019/03/11'
+        self.setUpTest(mock_path)
+        self.args.date_from = expected_date_from
+        self.args.date_to = expected_date_to
+        args_checker = ArgsChecker(self.args, self.args_default)
+
+        args_dict = args_checker.get_docs_mask_dict()
+
+        self.assertEqual(Timestamp(expected_date_from), args_dict['date_from'])
+        self.assertEqual(Timestamp(expected_date_to), args_dict['date_to'])
+
+    @mock.patch("scripts.utils.argschecker.path", create=True)
+    def test_get_docs_mask_dict_date_from_to_None(self, mock_path):
+        expected_date_from = '1900-01-01'
+        expected_date_to = pd.to_datetime('today').date()
+        self.setUpTest(mock_path)
+        self.args.date_from = None
+        self.args.date_to = None
+        args_checker = ArgsChecker(self.args, self.args_default)
+
+        args_dict = args_checker.get_docs_mask_dict()
+
+        self.assertEqual(Timestamp(expected_date_from), args_dict['date_from'])
+        self.assertEqual(expected_date_to, args_dict['date_to'])
