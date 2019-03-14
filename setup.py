8c129dc8252af0c1fab54b69c616487b343fe3e2
IanGrimstead <38883454+IanGrimstead@users.noreply.github.com>
2019-03-14 14:50:27 +0000

    added

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ def setup_package():
             'Programming Language :: Python :: 3.6',
         ],
         install_requires=['matplotlib', 'numpy', 'scipy', 'wordcloud', 'pandas', 'tqdm', 'nltk', 'scikit-learn', 'xlrd',
-                          'python-Levenshtein', 'gensim', 'pyramid-arima'],
+                          'python-Levenshtein', 'gensim', 'pyramid-arima', 'keras'],
         # extras_require={'dev': ['check-manifest'],'test': ['coverage'],},
         python_requires='>=3.6',
     )
