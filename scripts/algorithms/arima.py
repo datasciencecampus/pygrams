84556b5aca7656f3b8f446d455c833d9c2201fa6
IanGrimstead <38883454+IanGrimstead@users.noreply.github.com>
2019-03-22 09:56:11 +0000

    Removed erroneous 2nd arima fit (#212)
    arima fits on construction, don't need to explicitly call 'fit'

--- scripts/algorithms/arima.py
+++ scripts/algorithms/arima.py
@@ -17,8 +17,6 @@ class ARIMAForecast(object):
             error_action='ignore', suppress_warnings=True, stepwise=True
         )
 
-        #self.__stepwise_model.fit(data_in)
-
     @property
     def configuration(self):
         return self.__stepwise_model.order
