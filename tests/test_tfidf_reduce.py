a8ef0e4b797dea5882621fa6202c7ea28b4a7231
Thanasis <33151187+thanasions@users.noreply.github.com>
2019-04-09 11:53:37 +0100

    csv dates come as strings. Type-check to see what's going on and conv… (#232)
    * moved things around a bit. type check after df creaation inside not read from pickle clause. If read from pickle, that should have been taken care of..

--- tests/test_tfidf_reduce.py
+++ tests/test_tfidf_reduce.py
@@ -1,15 +1,7 @@
 import unittest
-
-import numpy as np
+import os
 import pandas as pd
-
-from scripts import FilePaths
-from scripts.filter_terms import FilterTerms
-from scripts.text_processing import StemTokenizer
-from scripts.tfidf_mask import TfidfMask
-from scripts.tfidf_reduce import TfidfReduce
-from scripts.tfidf_wrapper import TFIDF
-from scripts.utils import utils
+from scripts.pipeline import Pipeline
 from tests import support
 
 
@@ -21,53 +13,53 @@ class TestTfidfReduce(unittest.TestCase):
         max_n = 3
         max_df = 0.3
         ngram_range = (min_n, max_n)
-        df = pd.read_pickle(FilePaths.us_patents_random_100_pickle_name)
-        tfidf_obj = TFIDF(df['abstract'], ngram_range=ngram_range, max_document_frequency=max_df,
-                          tokenizer=StemTokenizer())
 
-        doc_weights = list(np.ones(len(df)))
+        date_to = pd.to_datetime('today').date()
+        date_from = pd.to_datetime('1900-01-01')
 
-        # term weights - embeddings
-        filter_output_obj = FilterTerms(tfidf_obj.feature_names, None, None)
-        term_weights = filter_output_obj.ngram_weights_vec
+        docs_mask_dict={}
+        docs_mask_dict['filter_by'] = 'union'
+        docs_mask_dict['cpc'] = None
+        docs_mask_dict['time'] = None
+        docs_mask_dict['cite'] = []
+        docs_mask_dict['columns'] = None
+        docs_mask_dict['date'] = {
+            'to': date_to,
+            'from': date_from
+        }
+        docs_mask_dict['date_header'] = 'publication_date'
 
-        tfidf_mask_obj = TfidfMask(tfidf_obj, ngram_range=ngram_range)
-        tfidf_mask_obj.update_mask(doc_weights, term_weights)
-        tfidf_mask = tfidf_mask_obj.tfidf_mask
+        filename = os.path.join('tests', 'data', 'USPTO-random-100.csv')
 
-        # mask the tfidf matrix
-        tfidf_matrix = tfidf_obj.tfidf_matrix
-        tfidf_masked = tfidf_mask.multiply(tfidf_matrix)
-        tfidf_masked = utils.remove_all_null_rows(tfidf_masked)
+        cls.__pipeline = Pipeline(filename, docs_mask_dict,  ngram_range=ngram_range,
+                  text_header='abstract', term_counts=True,
+                  max_df=max_df, output_name='test')
 
-        print(f'Processing TFIDF matrix of {tfidf_masked.shape[0]:,} / {tfidf_matrix.shape[0]:,} documents')
-
-        tfidf_reduce_obj = TfidfReduce(tfidf_masked, tfidf_obj.feature_names)
-        cls.__term_score_tuples = tfidf_reduce_obj.extract_ngrams_from_docset('sum')
+        cls.__term_score_tuples = cls.__pipeline.term_score_tuples
 
     def test_terms(self):
         term_score_tuples = self.__term_score_tuples
         actual_terms = [x for _, x in term_score_tuples]
-        expected_terms = ['mount surfac',
+        expected_terms = ['mounting surface',
                           'transmit path',
-                          'electron element',
+                          'electronic element',
                           'link document',
-                          'amid deriv',
+                          'amide derivative',
                           'valproic acid',
-                          'voic messag',
-                          'pharmaceut formul',
-                          'jack mechan',
+                          'voice message',
+                          'jack mechanism',
+                          'pharmaceutical formulation',
                           'light beam',
+                          'angular velocity',
                           'contact beam',
-                          'angular veloc',
-                          'shorter tuft',
-                          'conduct materi',
-                          'endodont instrument',
+                          'conductive material',
+                          'endodontic instrument',
                           'mass offset',
                           'section bend',
-                          'compon materi',
-                          'connect portion',
-                          'termin channel'
+                          'component material',
+                          'terminal channel',
+                          'stationary household appliance',
+                          'fault point'
                           ]
 
         self.assertListEqual(actual_terms[:20], expected_terms)
@@ -75,25 +67,29 @@ class TestTfidfReduce(unittest.TestCase):
     def test_scores(self):
         term_score_tuples = self.__term_score_tuples
         actual_scores = [x for x, _ in term_score_tuples]
-        expected_scores = [0.9449111825230679,
+        expected_scores = [0.8728715609439694,
                            0.8259734063804905,
                            0.7754588414852185,
-                           0.7276068751089988,
+                           0.7620007620011429,
                            0.7071067811865476,
                            0.7071067811865476,
                            0.7071067811865475,
+                           0.6882472016116852,
                            0.6666666666666666,
-                           0.6396021490668312,
                            0.6246950475544241,
+                           0.6198903382379372,
                            0.6031800939323297,
-                           0.6000595413031171,
-                           0.5834599659915781,
                            0.5806718350868961,
                            0.5773502691896257,
                            0.5773502691896257,
                            0.5773502691896257,
                            0.5669467095138407,
-                           0.5611088299627696,
-                           0.5597177778726654]
+                           0.5597177778726654,
+                           0.5570860145311556,
+                           0.5568900989230109]
 
         support.assert_list_almost_equal(self, actual_scores[:20], expected_scores)
+
+    def test_timeseries_mat(self):
+        timeseries_mat = self.__pipeline.term_counts_data
+        self.assertEqual(sum(timeseries_mat[2]), 100)
