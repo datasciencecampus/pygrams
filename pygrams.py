9bb15a21bfacccae09dc70e01ece594b8178f02a
Thanasis <33151187+thanasions@users.noreply.github.com>
2019-05-22 10:52:08 +0100

    279 small adjustments (#280)
    * Dates now pickled as integer array to save space (#273)
    * Tidied up date related utilities - added to date_utils from utils
    * Renamed 'iso dates' to 'year_week' dates to avoid confusion with 'real' iso
    * Column filter removed from DocumentsFilter
    * Removed time and CPC document weighting
    * Removed unused parameters and synchronised variable names (#273)
    * Added timing report and progress reports

--- pygrams.py
+++ pygrams.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 import sys
+import time
 
 from scripts.pipeline import Pipeline
 from scripts.utils.argschecker import ArgsChecker
@@ -141,6 +142,7 @@ def get_args(command_line_arguments):
 
 
 def main(supplied_args):
+
     paths = [os.path.join('outputs', 'reports'), os.path.join('outputs', 'wordclouds'),
              os.path.join('outputs', 'table'), os.path.join('outputs', 'emergence')]
     for path in paths:
@@ -242,6 +244,15 @@ def main(supplied_args):
 
 if __name__ == '__main__':
     try:
+        start = time.time()
         main(sys.argv[1:])
+        end = time.time()
+        diff = int(end - start)
+        hours=diff//3600
+        minutes=diff//60
+        seconds=diff%60
+
+        print('')
+        print(f"pyGrams query took {hours}:{minutes:02d}:{seconds:02d} to complete")
     except PygramsException as err:
         print(f"pyGrams error: {err.message}")
