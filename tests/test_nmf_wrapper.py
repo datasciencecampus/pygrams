edabd36e57ac8cfeef32d10315425e63ab0b6d1b
Thanasis <33151187+thanasions@users.noreply.github.com>
2019-10-15 11:30:38 +0100

    337 stop n (#341)
    * tests pass. Tokens blocked across punctuation, but include stopwords, like 'internet of things'

--- tests/test_nmf_wrapper.py
+++ tests/test_nmf_wrapper.py
@@ -21,31 +21,31 @@ class TestNMFWrapper(unittest.TestCase):
         cls.__nmf = nmf_topic_modelling(nmf_topics, tfidf_obj.tfidf_matrix)
 
     def test_nmf_topic1(self):
-        actual_topic_1_score = self.__nmf.components_[0][3302]
-        expected_topic_1_score = 0.2044937886411859
+        actual_topic_1_score = self.__nmf.components_[0][1399]
+        expected_topic_1_score = 0.2061
 
         assert_almost_equal(actual_topic_1_score, expected_topic_1_score, decimal=3)
 
     def test_nmf_topic2(self):
-        actual_topic_2_score = self.__nmf.components_[1][281]
-        expected_topic_2_score = 0.276781
+        actual_topic_2_score = self.__nmf.components_[1][2886]
+        expected_topic_2_score = 0.0234
 
         assert_almost_equal(actual_topic_2_score, expected_topic_2_score, decimal=3)
 
     def test_nmf_topic3(self):
-        actual_topic_3_score = self.__nmf.components_[2][2983]
-        expected_topic_3_score = 0.441
+        actual_topic_3_score = self.__nmf.components_[2][8495]
+        expected_topic_3_score = 0.1609814198116909
 
         assert_almost_equal(actual_topic_3_score, expected_topic_3_score, decimal=3)
 
     def test_nmf_topic4(self):
-        actual_topic_4_score = self.__nmf.components_[3][1683]
-        expected_topic_4_score = 0.219
+        actual_topic_4_score = self.__nmf.components_[3][3983]
+        expected_topic_4_score = 0.1669984704619599
 
         assert_almost_equal(actual_topic_4_score, expected_topic_4_score, decimal=3)
 
     def test_nmf_topic5(self):
-        actual_topic_5_score = self.__nmf.components_[4][252]
-        expected_topic_5_score = 0.275
-
+        actual_topic_5_score = self.__nmf.components_[4][7621]
+        expected_topic_5_score = 0.2443243734131224
         assert_almost_equal(actual_topic_5_score, expected_topic_5_score, decimal=3)
+
