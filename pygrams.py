84495581c6fadb84873fde232ac67b07884474c4
Thanasis <thanasis.anthopoulos@ons.gov.uk>
2019-10-31 10:42:32 +0000

    improved outputs

--- pygrams.py
+++ pygrams.py
@@ -247,7 +247,7 @@ def main(supplied_args):
                 </html>
                 '''
 
-            base_file_name = os.path.join('outputs', 'emergence', args.outputs_name + '_' + emergence)
+            base_file_name = os.path.join(dir_path, args.outputs_name + '_' + emergence)
 
             if args.normalised:
                 base_file_name += '_normalised'
