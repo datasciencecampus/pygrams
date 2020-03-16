4cd57fa7b0b00a6525bca737c42e37bc87daceae
IanGrimstead <Ian.Grimstead@ons.gov.uk>
2020-03-16 10:29:48 +0000

    #347 potentially fixes headless issues with Tk (allegedly matplotlib triggers it)

--- scripts/visualization/wordclouds/multicloudplot.py
+++ scripts/visualization/wordclouds/multicloudplot.py
@@ -1,5 +1,7 @@
-import matplotlib
-matplotlib.use('TkAgg')
+from sys import platform as sys_pf
+if sys_pf == 'darwin':
+    import matplotlib
+    matplotlib.use("TkAgg")
 
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud
